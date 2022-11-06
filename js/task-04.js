3fbe96c4d53d848a6cc85a7fcc3f8b55720352e4
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2022-11-06 03:20:29 +0200

    task-4 second method

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,51 @@
+const value = document.querySelector("#value");
+const decrementBtn = document.querySelector(
+  '#counter button[data-action = "decrement"]'
+);
+const incrementBtn = document.querySelector(
+  '#counter button[data-action = "increment"]'
+);
+
+function counterValue(currentCounter = 0) {
+  decrementBtn.addEventListener("click", () => {
+    currentCounter -= 1;
+    value.textContent = currentCounter;
+  });
+  incrementBtn.addEventListener("click", () => {
+    currentCounter += 1;
+    value.textContent = currentCounter;
+  });
+}
+
+counterValue();
+
+// class Counter {
+//   constructor(selector, initialValue) {
+//     this.wrapper = document.querySelector(selector);
+//     this.initialValue = initialValue;
+//     this.addListeners();
+//   }
+
+//   addListeners = () => {
+//     this.wrapper
+//       .querySelector('[data-action="decrement"]')
+//       .addEventListener("click", this.handleDecrement);
+//     this.wrapper
+//       .querySelector('[data-action="increment"]')
+//       .addEventListener("click", this.handleIncrement);
+//   };
+//   handleDecrement = () => {
+//     this.initialValue -= 1;
+//     this.wrapper.querySelector("#value").textContent = this.initialValue;
+//   };
+//   handleIncrement = () => {
+//     this.initialValue += 1;
+//     this.wrapper.querySelector("#value").textContent = this.initialValue;
+//   };
+// }
+
+// new Counter("#counter", 0);
+
 // Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать и уменьшать его
 //  значение на единицу.
 
@@ -12,33 +60,3 @@
 // Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай
 // значение счтетчика.
 // Обновляй интерфейс новым значением переменной counterValue.
-// const buttons = document.querySelectorAll("#counter button");
-// const btnDecrement = buttons[0].dataset;
-// const btnIncrement = buttons[1].dataset.action;
-
-class Counter {
-  constructor(selector, initialValue) {
-    this.wrapper = document.querySelector(selector);
-    this.initialValue = initialValue;
-    this.addListeners();
-  }
-
-  addListeners = () => {
-    this.wrapper
-      .querySelector('[data-action="decrement"]')
-      .addEventListener("click", this.handleDecrement);
-    this.wrapper
-      .querySelector('[data-action="increment"]')
-      .addEventListener("click", this.handleIncrement);
-  };
-  handleDecrement = () => {
-    this.initialValue -= 1;
-    this.wrapper.querySelector("#value").textContent = this.initialValue;
-  };
-  handleIncrement = () => {
-    this.initialValue += 1;
-    this.wrapper.querySelector("#value").textContent = this.initialValue;
-  };
-}
-
-new Counter("#counter", 0);
